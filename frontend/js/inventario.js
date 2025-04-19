6011c7f625bab1c14571d469fdbc69fa23714f24
cristianguerrero18 <guerrerrosilvacristian@gmail.com>
2025-04-18 20:00:12 -0500

    visualizacion de las tablas con el consumo de api cristian guerrero y fabian parada

---
+++ frontend/js/inventario.js
@@ -0,0 +1,40 @@
+import {obtainCategories} from "../apiConnection/consumeApi.js"
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    getCategories();
+})
+
+async function getCategories(){
+    const categoriesObtained = await obtainCategories(); 
+const container = document.querySelector('tbody')
+categoriesObtained.forEach((category)=>{
+    const {CategoriaID, CategoriaNombre, Descripcion, Imagen} = category
+    const row  = document.createElement('tr');
+    row.innerHTML =  `
+    <td>
+    ${CategoriaID}
+    </td>
+    <td>
+    ${CategoriaNombre}
+    </td>
+    <td>
+    ${Descripcion}
+    </td>
+    <td><img src="${Imagen}" width="100px"></td>
+    <td>
+    <button class="btn color2">Details</button>
+    </td>
+    <td>
+    <button class="btn color3">Update</button>
+    </td>
+    <td>
+    <button class="btn color5">Delete</button>
+    </td>
+    
+    `;
+    container.appendChild(row)
+})
+}
+
+
